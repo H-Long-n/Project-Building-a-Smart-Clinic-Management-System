49f84949f1ae6f6c87e4bf2405ff75c29e3d5858
H-Long-n <shenlong2035@gmail.com>
2025-10-25 12:19:24 +0700

    Add files via upload

---
+++ clinic-appointment-system-main/clinic-appointment-system-main/src/main/java/com/example/clinic/model/Doctor.java
@@ -0,0 +1,50 @@
+package com.example.clinic.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long doctorId;
+
+    private String name;
+
+    private String specialization;
+
+    @ElementCollection
+    private List<String> availableTimes; // ex: ["2025-09-20T10:00", "2025-09-21T14:00"]
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public List<String> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public void setAvailableTimes(List<String> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+}
